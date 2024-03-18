4776a9e5ed4242b4cd6893bfe3d344e9d0fa9bc9
Swati Pandey <swati.p@prograd.org>
2024-03-18 12:57:56 +0530

    Edit TotalCommission file

--- TypeScript-Labs/1-lab-if-else-solution/src/TotalCommission.js
+++ TypeScript-Labs/1-lab-if-else-solution/src/TotalCommission.js
@@ -2,6 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getTotalCommission = void 0;
 function getTotalCommission(sales) {
+    var commission;
+    if (sales > 50000) {
+        commission = sales * 0.30;
+    }
+    else if (sales > 40000) {
+        commission = sales * 0.20;
+    }
+    else if (sales > 30000) {
+        commission = sales * 0.15;
+    }
+    else if (sales > 20000) {
+        commission = sales * 0.10;
+    }
+    else if (sales > 10000) {
+        commission = sales * 0.08;
+    }
+    else if (sales >= 0) {
+        commission = sales * 0.05;
+    }
+    else {
+        commission = -1;
+    }
     return commission;
 }
 exports.getTotalCommission = getTotalCommission;
