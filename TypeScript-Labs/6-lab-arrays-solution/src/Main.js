cd258073c7d2130f2c7593f8e113ca8dfca03b3c
Swati Pandey <swati.p@prograd.org>
2024-03-18 13:53:10 +0530

    Edit Main file

--- TypeScript-Labs/6-lab-arrays-solution/src/Main.js
+++ TypeScript-Labs/6-lab-arrays-solution/src/Main.js
@@ -3,10 +3,24 @@ var GroceryStore = /** @class */ (function () {
         this.groceryList = [];
     }
     GroceryStore.prototype.addItem = function (item) {
+        this.groceryList.push(item);
+        console.log("".concat(item, " has/have been Added to the Grocery List."));
     };
     GroceryStore.prototype.removeItem = function (item) {
+        var index = this.groceryList.indexOf(item);
+        if (index !== -1) {
+            this.groceryList.splice(index, 1);
+            console.log("".concat(item, " has/have been Removed from the Grocery List."));
+        }
+        else {
+            console.log("".concat(item, " is not in the Grocery List."));
+        }
     };
     GroceryStore.prototype.displayList = function () {
+        console.log("Grocery List:");
+        this.groceryList.forEach(function (item, index) {
+            console.log("".concat(index + 1, ". ").concat(item));
+        });
     };
     return GroceryStore;
 }());
--- TypeScript-Labs/6-lab-arrays-solution/src/Main.ts
+++ TypeScript-Labs/6-lab-arrays-solution/src/Main.ts
@@ -6,15 +6,25 @@ class GroceryStore {
     }
 
     addItem(item: string): void {
-
+        this.groceryList.push(item);
+        console.log(`${item} has/have been Added to the Grocery List.`);
     }
 
     removeItem(item: string): void {
-
+        const index = this.groceryList.indexOf(item);
+        if (index !== -1) {
+            this.groceryList.splice(index, 1);
+            console.log(`${item} has/have been Removed from the Grocery List.`);
+        } else {
+            console.log(`${item} is not in the Grocery List.`);
+        }
     }
 
     displayList(): void {
-
+        console.log("Grocery List:");
+        this.groceryList.forEach((item, index) => {
+            console.log(`${index + 1}. ${item}`);
+        });
     }
 }
 
