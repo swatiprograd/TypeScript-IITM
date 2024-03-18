b04ae0ac50961cf6b7e6d25df8a3dddec16d829c
Swati Pandey <swati.p@prograd.org>
2024-03-18 13:23:11 +0530

    Edit CompoundAmount file

--- TypeScript-Labs/2-lab-switch-solution/src/CompoundAmount.js
+++ TypeScript-Labs/2-lab-switch-solution/src/CompoundAmount.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getCompoundAmount = void 0;
 function getCompoundAmount(principal, time, rate) {
+    var compoundAmount;
+    compoundAmount = principal * Math.pow((1 + rate / 100), time);
     return compoundAmount;
 }
 exports.getCompoundAmount = getCompoundAmount;
--- TypeScript-Labs/2-lab-switch-solution/src/CompoundAmount.ts
+++ TypeScript-Labs/2-lab-switch-solution/src/CompoundAmount.ts
@@ -1,4 +1,6 @@
 export function getCompoundAmount(principal: number, time: number, rate: number): number {
+    let compoundAmount: number;
 
+    compoundAmount = principal * Math.pow((1 + rate / 100), time);
     return compoundAmount;
 }
