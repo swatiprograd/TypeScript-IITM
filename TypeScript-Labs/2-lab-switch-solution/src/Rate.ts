4985dd53ed50fb9d30cacf2cdb0e98906179b372
Swati Pandey <swati.p@prograd.org>
2024-03-18 13:21:57 +0530

    Edit Rate file

--- TypeScript-Labs/2-lab-switch-solution/src/Rate.ts
+++ TypeScript-Labs/2-lab-switch-solution/src/Rate.ts
@@ -1,4 +1,26 @@
 export function getRate(principal: number, time: number): number {
+    let rate: number;
 
+    if (principal < 25000) {
+        rate = 6.0;
+    } else {
+        switch (time) {
+            case 7:
+                rate = 7.0;
+                break;
+            case 10:
+                rate = 8.5;
+                break;
+            case 15:
+                rate = 9.5;
+                break;
+            case 20:
+                rate = 12.5;
+                break;
+            default:
+                rate = 6.75;
+                break;
+        }
+    }
     return rate;
 }
